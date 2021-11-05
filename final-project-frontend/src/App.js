c02795cd2b90dd3c452d48e0b0c21b9a30907e70
Branden <brandenehman45@gmail.com>
2021-11-04 18:12:17 -0600

    Added some beginning things to the app.js file

--- final-project-frontend/src/App.js
+++ final-project-frontend/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import React, {useState, useEffect} from "react"
+import WelcomePage from './components/WelcomePage';
+import BeehivesContainer from './components/BeehivesContainer';
+import OwnersContainer from './components/OwnersContainer';
+import NavBar from './components/NavBar';
+import BeehiveForm from './components/BeehiveForm';
 
 function App() {
+
+  const [beehives, setBeehives] = useState([])
+  
   return (
     <div className="App">
-      
+      <Router>
+        <NavBar/>
+        <Switch>
+          <Route path="/owners" ><OwnersContainer/></Route>
+          <Route path="/beehives" ><BeehivesContainer beehives={beehives} requesting={requesting} /></Route>
+          <Route path="/beehiveform"><BeehiveForm /></Route>
+          <Route exact path="/" ><WelcomePage/></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
