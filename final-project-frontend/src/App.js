68fa94f28e4e120d960da6d1d7465dc3c2af19c1
Branden <brandenehman45@gmail.com>
2021-11-11 16:52:19 -0700

    Took all the fetch requests and functions from app.js and placed them all in BeehiveContainer

--- final-project-frontend/src/App.js
+++ final-project-frontend/src/App.js
@@ -10,69 +10,6 @@ import BeehiveInfo from './components/BeehiveInfo';
 
 function App() {
 
-  const [beehives, setBeehives] = useState([])
-
-  useEffect(() => {
-    fetch("http://localhost:9292/beehives")
-    .then(resp => resp.json())
-    .then(data => setBeehives([...data]))
-  }, []);
-
-  const addNewBeehive = (beehiveData) => {
-    let params = {...beehiveData}
-    fetch("http://localhost:9292/beehives", {
-      method: "POST",
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(params)
-    })
-    .then(resp => resp.json())
-    .then(beehiveData => {
-      setBeehives(prev => {
-        return [...prev, beehiveData]
-      })
-    })
-  }
-
-  const handleDelete = async (id) => {
-    let resp = await fetch(`http://localhost:9292/beehives/${id}`, {
-      method: "DELETE",
-      headers: {
-          "Accept": "application/json",
-          "Content-Type": "application/json"
-      }
-    })
-    let beehiveData = await resp.json() 
-    let beehiveAfterDelete = beehives.filter(beehive => beehive.id !== id)
-    setBeehives([...beehiveAfterDelete])
-  }
-
-  const handleLike = async (beehive) => {
-    let likedBeehive = beehives.map(hive => {
-      if(hive.id === beehive.id){
-        hive.likes = beehive.likes + 1
-        return hive
-      } else {
-        return hive
-      }
-    })
-    let params = {
-      likes: beehive.likes
-    }
-    setBeehives([...likedBeehive])
-    let resp = await fetch(`http://localhost:9292/beehives/${beehive.id}`, {
-        method: "PATCH",
-        headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(params)
-    })
-    let beehiveData = await resp.json()
-  }
-
   return (
     <div className="App">
       <Router>
--- final-project-frontend/src/components/BeehivesContainer.jsx
+++ final-project-frontend/src/components/BeehivesContainer.jsx
@@ -1,7 +1,70 @@
 import React from "react";
 import BeehiveCard from "./BeehiveCard"
 
-function BeehivesContainer({ beehives, handleDelete, handleLike }) {
+function BeehivesContainer() {
+
+  const [beehives, setBeehives] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:9292/beehives")
+    .then(resp => resp.json())
+    .then(data => setBeehives([...data]))
+  }, []);
+
+  const addNewBeehive = (beehiveData) => {
+    let params = {...beehiveData}
+    fetch("http://localhost:9292/beehives", {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(params)
+    })
+    .then(resp => resp.json())
+    .then(beehiveData => {
+      setBeehives(prev => {
+        return [...prev, beehiveData]
+      })
+    })
+  }
+
+  const handleDelete = async (id) => {
+    let beehivePostDelete = beehives.filter(beehive => beehive.id !== id)
+    setBeehives([...beehivePostDelete])
+    let resp = await fetch(`http://localhost:9292/beehives/${id}`, {
+      method: "DELETE",
+      headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+      }
+    })
+  }
+
+  const handleLike = async (beehive) => {
+    let likedBeehive = beehives.map(hive => {
+      if(hive.id === beehive.id){
+        hive.likes = beehive.likes + 1
+        return hive
+      } else {
+        return hive
+      }
+    })
+    let params = {
+      likes: beehive.likes
+    }
+    setBeehives([...likedBeehive])
+    let resp = await fetch(`http://localhost:9292/beehives/${beehive.id}`, {
+        method: "PATCH",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(params)
+    })
+    let beehiveData = await resp.json()
+  }
+
     return (
         <div>
             <h1>Beehives</h1>
