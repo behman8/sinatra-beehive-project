6510a897f49f08e93761d1d5ded3dcdf1b888897
Branden <brandenehman45@gmail.com>
2021-11-11 18:45:09 -0700

    Added fetch to owner container to display info on the owners

--- final-project-frontend/src/components/OwnersContainer.jsx
+++ final-project-frontend/src/components/OwnersContainer.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 function OwnersContainer() {
+
+    const [owner, setOwner] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:9292/beehives")
+        .then(resp => resp.json())
+        .then(data => {
+            setOwner([...data])
+        })
+    }, [])
+
     return (
         <div>
             <h1>Owners</h1>
